c283aa6a15ea07e93fe7422e91be755ef9a9fb17
sachchidanandY <sachchidanandy@mindfiresolutions.com>
2019-03-12 13:06:52 +0530

    Parsing Payloads

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
  //Dependencies
  const http = require('http');
  const url  = require('url');
+ const StringDecoder = require('string_decoder').StringDecoder;
 
  //Creating Server
  const server = http.createServer((req, res) => {
@@ -27,13 +28,29 @@
 
     //Get headers
     const headers = req.headers;
-    
-    //Sending response
-    res.end('Hello From Sachin\n');
 
-    //Log
-    console.log(`Request Path: ${trimmedPath}\nRequest Method: ${method}\nRequest Query String Object:`, queryStringObject, `\nRequest Headers:`, headers);
+    //Get the payload if any (using utf-8 decoder)
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    //Adding a data event listener which will listen to stream data comming
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+    //Adding a end event listener which will be called every time
+    req.on('end', () => {
+        buffer += decoder.end();
 
+        //Sending response
+        res.end('Hello From Sachin\n');
+
+        //Log
+        console.log('Request Path: ', trimmedPath,
+            '\nRequest Method: ', method,
+            '\nRequest Query String Object: ', queryStringObject,
+            '\nRequest Headers: ', headers,
+            '\nRequest Buffer (payload or body): ', buffer
+        );
+    });
  });
 
  //Starting server
